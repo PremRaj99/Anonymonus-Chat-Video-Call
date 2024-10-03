8f8945c39e53282a8bfe05a80030ea29c04145d7
Prem Raj <premraj299341@gmail.com>
2024-10-03 20:23:46 +0530

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+## Amoymanous Chat | video call using WebRTC (P2P)
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <div className="bg-black text-white px-40 py-4 text-xl">App</div>
+  )
+}
